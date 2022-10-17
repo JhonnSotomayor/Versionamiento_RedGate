ceb911e9b00df59e341507d0a4ce236d38327375
Jhonn Sotomayor <jhonn.sotomayor@vallegrande.edu.pe>
2022-10-17 14:05:34 -0500

    Creación de cuatro base de datos

---
+++ Data/dbo.EXAMPLE22_Data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[EXAMPLE22] ON
+INSERT INTO [dbo].[EXAMPLE22] ([CODEMP], [NOMEMP], [APEEMP], [DNIEMP], [EMAEMP], [CELEMP]) VALUES (1, 'juan', 'Porras Ortega', '99564865', 'juan@gmail.com', '928794535')
+INSERT INTO [dbo].[EXAMPLE22] ([CODEMP], [NOMEMP], [APEEMP], [DNIEMP], [EMAEMP], [CELEMP]) VALUES (2, 'Pedro', 'Porras Luyo', '12345678', 'pedro@gmail.com', '978506127')
+INSERT INTO [dbo].[EXAMPLE22] ([CODEMP], [NOMEMP], [APEEMP], [DNIEMP], [EMAEMP], [CELEMP]) VALUES (3, 'Julian', 'Talla Sandoval', '12445678', 'julian@gmail.com', '990456124')
+INSERT INTO [dbo].[EXAMPLE22] ([CODEMP], [NOMEMP], [APEEMP], [DNIEMP], [EMAEMP], [CELEMP]) VALUES (4, 'Maximo', 'Ramirez Damian', '19346678', 'max@gmail.com', '978456150')
+INSERT INTO [dbo].[EXAMPLE22] ([CODEMP], [NOMEMP], [APEEMP], [DNIEMP], [EMAEMP], [CELEMP]) VALUES (5, 'Luis', 'Juscamayta Torres', '12245678', 'Luis@gmail.com', '978470120')
+INSERT INTO [dbo].[EXAMPLE22] ([CODEMP], [NOMEMP], [APEEMP], [DNIEMP], [EMAEMP], [CELEMP]) VALUES (6, 'Raquel', 'Huaman Arias', '12345778', 'raquel@gmail.com', '922456122')
+SET IDENTITY_INSERT [dbo].[EXAMPLE22] OFF
